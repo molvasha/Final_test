22230a39343226a13e2f58ed275e2c2f017e6b8e
molvasha <64091616+molvasha@users.noreply.github.com>
2023-10-30 13:24:20 +0300

    Add files via upload
    the main program

---
+++ final.cs
@@ -0,0 +1,44 @@
+﻿﻿Console.WriteLine("Введите элементы массива через запятую:");
+string[] inputArray = EnterArray();
+
+string[] resultArray = FilterArray(inputArray);
+
+Console.WriteLine("Результат:");
+PrintArray(inputArray, resultArray);
+
+string[] EnterArray()
+{
+    string input = Console.ReadLine();
+    return input.Split(',');
+}
+
+string[] FilterArray(string[] inputArray)
+{
+    int count = 0;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] resultArray = new string[count];
+    int j = 0;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            resultArray[j] = inputArray[i];
+            j++;
+        }
+    }
+
+    return resultArray;
+}
+
+void PrintArray(string[] inputArray, string[] resultArray)
+{
+    Console.Write("[" + string.Join(", ", inputArray) + "] -> ");
+    Console.WriteLine("[" + string.Join(", ", resultArray) + "]");
+}
